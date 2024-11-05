27070c3c23fa9888841d646bb69986dfbf697362
khairulharu <khairulaswad719@gmail.com>
2024-11-05 13:15:31 +0800

    learn: modifier on contract

---
+++ contracts/EnhancedBank.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract EnhancedBank {
+    address public owner;
+    mapping(address => uint) public balances;
+
+    constructor() public {
+      owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+      require(msg.sender == owner, "Not the contract owner");
+    }
+
+    function deposit() public payable {
+      balances[msg.sender] += msg.value;
+    }
+
+    function withDraw(uint amount) public {
+      require(balances[msg.sender] >= amount, "Insuficient balance");
+      payable(msg.sender).transfer(amount);
+      balances[msg.sender] -= amount;
+    }
+
+    function getBalances() public view returns (uint) {
+      return balances[msg.sender];
+    }
+
+    function emergencyWithDraw() public onlyOwner {
+      payable(owner).transfer(address(this).balance);
+    }
+}
