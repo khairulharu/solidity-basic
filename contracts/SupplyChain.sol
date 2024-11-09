6ee5bdf11f78e8de2ae3983d97e38264b2f9c2d1
khairulharu <khairulaswad719@gmail.com>
2024-11-09 20:02:04 +0800

    define a fields that need for supply chain contract

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.19;
 
 contract SupplyChain {
-  
+  enum Status {
+    Pending,
+    Shipped,
+    Delivered,
+    Cancelled
+  }
+
+  struct Order {
+    uint id;
+    string item;
+    uint quantity;
+    Status status;
+  }
+
+  Order[] public orders;
 }
